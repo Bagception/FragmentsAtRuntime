25a0d856f84ce430a430bfcb85e1a8ca9cea616e
Philipp Hock <mail@philipphock.de>
2013-11-04 11:20:42 +0100

    2 pane layout

--- src/de/philipphock/android/fragmentsatruntime/MainActivity.java
+++ src/de/philipphock/android/fragmentsatruntime/MainActivity.java
@@ -1,23 +1,56 @@
 package de.philipphock.android.fragmentsatruntime;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements HeadlinesFragment.OnHeadlineSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if(findViewById(R.id.fragment_container) != null){
+        	if (savedInstanceState != null){
+        		//restored, the fragment exists
+        		return;
+        	}
+        	
+        	
+        	HeadlinesFragment firstElement = new HeadlinesFragment();
+        	firstElement.setArguments(getIntent().getExtras());
+        	getFragmentManager().beginTransaction()
+        		.add(R.id.fragment_container, firstElement).commit();
+        	
+        	
+        	
+        }else{
+        	
+        }
     }
 
+	@Override
+	public void onArticleSelected(int position) {
+		
+		ArticleFragment articleFrag = (ArticleFragment) getFragmentManager().findFragmentById(R.id.article_fragment);
+		if (articleFrag!=null){
+			//large layout
+			articleFrag.updateArticleView(position);
+		}else{
+			//small layout
+			ArticleFragment newFragment = new ArticleFragment();
+			Bundle args = new Bundle();
+			args.putInt(ArticleFragment.ARG_POSITION, position);
+			newFragment.setArguments(args);
+			
+			FragmentTransaction trans = getFragmentManager().beginTransaction();
+			trans.replace(R.id.fragment_container, newFragment);
+			trans.addToBackStack(null);
+			trans.commit();
+		}
+		}
+		
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }
